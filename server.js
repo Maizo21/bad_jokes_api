f2954293c1144a35d02f03fb216d01ef295d731c
Hernan Amaiz <66435038+Maizo21@users.noreply.github.com>
2025-10-07 19:07:59 -0300

    Correcciones y creacion de index

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
 const PORT = process.env.PORT || 8000;
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json()); 
 const chistesRouter = require('./routes/chistes');
